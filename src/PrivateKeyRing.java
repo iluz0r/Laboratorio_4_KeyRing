66aa2c0eada531df5948f55a878b92599d16bfc0
Angelo S. <boyangel90@hotmail.it>
2016-12-09 03:43:07 +0100

    getKey(alias) done!

--- src/PrivateKeyRing.java
+++ src/PrivateKeyRing.java
@@ -1,6 +1,13 @@
+import java.security.Key;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PrivateKeyRing {
 
@@ -34,46 +41,82 @@ public class PrivateKeyRing {
 
 	private static PrivateKeyRing instance = null;
 	private static final String groupName = "I2X3";
+	private List<Record> keyRing;
 
-	private PrivateKeyRing() {
+	private PrivateKeyRing() throws InvalidKeySpecException, NoSuchAlgorithmException {
+		keyRing = new ArrayList<>();
+
 		KeyPair keyPairRSA, keyPairDSA;
-		try {
-			KeyPairGenerator keyPairGenRSA = KeyPairGenerator.getInstance("RSA");
-			keyPairGenRSA.initialize(1024);
-			keyPairRSA = keyPairGenRSA.genKeyPair();
+		KeyPairGenerator keyPairGenRSA = KeyPairGenerator.getInstance("RSA");
+		keyPairGenRSA.initialize(1024);
+		keyPairRSA = keyPairGenRSA.genKeyPair();
 
-			KeyPairGenerator keyPairGenDSA = KeyPairGenerator.getInstance("DSA");
-			keyPairGenDSA.initialize(1024);
-			keyPairDSA = keyPairGenDSA.genKeyPair();
+		KeyPairGenerator keyPairGenDSA = KeyPairGenerator.getInstance("DSA");
+		keyPairGenDSA.initialize(1024);
+		keyPairDSA = keyPairGenDSA.genKeyPair();
 
-			// EPK = public encryption key
-			new Record(groupName + "_EPK", keyPairRSA.getPublic().getEncoded());
-			// ESK = private decryption key
-			new Record(groupName + "_ESK", keyPairRSA.getPrivate().getEncoded());
-			// SPK = public verification key
-			new Record(groupName + "_SPK", keyPairDSA.getPublic().getEncoded());
-			// SSK = private signing key
-			new Record(groupName + "_SSK", keyPairDSA.getPrivate().getEncoded());
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
+		// EPK = public encryption key
+		keyRing.add(new Record(groupName + "_EPK", keyPairRSA.getPublic().getEncoded(), "EPK"));
+		// ESK = private decryption key
+		keyRing.add(new Record(groupName + "_ESK", keyPairRSA.getPrivate().getEncoded(), "ESK"));
+		// SPK = public verification key
+		keyRing.add(new Record(groupName + "_SPK", keyPairDSA.getPublic().getEncoded(), "SPK"));
+		// SSK = private signing key
+		keyRing.add(new Record(groupName + "_SSK", keyPairDSA.getPrivate().getEncoded(), "SSK"));
 	}
 
-	public static PrivateKeyRing getInstance() {
+	public static PrivateKeyRing getInstance() throws InvalidKeySpecException, NoSuchAlgorithmException {
 		if (instance == null)
 			instance = new PrivateKeyRing();
 		return instance;
+	}
+
+	public Key getKey(String alias) throws Exception {
+		byte[] encodedKey = null;
+		String keyType = null;
+		for (Record r : keyRing) {
+			if (r.getAlias().equals(alias)) {
+				encodedKey = r.getEncodedKey();
+				keyType = r.getKeyType();
+			}
+		}
+
+		if (encodedKey == null)
+			throw new Exception("Key not found!");
+
+		Key key = null;
+		if (keyType.equals("EPK") || keyType.equals("ESK")) {
+			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			if (keyType.equals("EPK"))
+				key = keyFactory.generatePublic(new X509EncodedKeySpec(encodedKey));
+			else
+				key = keyFactory.generatePrivate(new PKCS8EncodedKeySpec(encodedKey));
+		}
+		if (keyType.equals("SPK") || keyType.equals("SSK")) {
+			KeyFactory keyFactory = KeyFactory.getInstance("DSA");
+			if (keyType.equals("SPK"))
+				key = keyFactory.generatePublic(new X509EncodedKeySpec(encodedKey));
+			else
+				key = keyFactory.generatePrivate(new PKCS8EncodedKeySpec(encodedKey));
+		}
+
+		if (key == null)
+			throw new Exception("Invalid key type!");
+
+		return key;
 	}
 
 	/* Nested class */
 	public static final class Record {
 
 		private String alias;
-		private byte[] encodedKey;
+		private byte[] encodedKey; // chiave trasparente
+		private String keyType;
 
-		public Record(String alias, byte[] encodedKey) {
+		public Record(String alias, byte[] encodedKey, String keyType) {
 			this.alias = alias;
 			this.encodedKey = encodedKey;
+			this.keyType = keyType;
 		}
 
 		public String getAlias() {
@@ -82,6 +125,10 @@ public class PrivateKeyRing {
 
 		public byte[] getEncodedKey() {
 			return encodedKey;
+		}
+
+		public String getKeyType() {
+			return keyType;
 		}
 
 	}
--- src/Run.java
+++ src/Run.java
@@ -1,9 +1,8 @@
-
 public class Run {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		PrivateKeyRing pkr = PrivateKeyRing.getInstance();
-		
+		System.out.println(pkr.getKey("I2X3_EPK").getEncoded());
 	}
 
 }
