a0326a78085f1aecb0694cd99168955539fbe6d1
Angelo S. <boyangel90@hotmail.it>
2016-12-09 04:30:55 +0100

    Code clean

--- src/Run.java
+++ src/Run.java
@@ -8,23 +8,23 @@ public class Run {
 	public static void main(String[] args) throws Exception {
 		PrivateKeyRing pkr = PrivateKeyRing.getInstance();
 
-		KeyPair keyPairRSA, keyPairDSA;
-		KeyPairGenerator keyPairGenRSA = KeyPairGenerator.getInstance("RSA");
-		keyPairGenRSA.initialize(1024);
-		keyPairRSA = keyPairGenRSA.genKeyPair();
+		KeyPair RSAKeyPair, DSAKeyPair;
+		KeyPairGenerator RSAKeyPairGen = KeyPairGenerator.getInstance("RSA");
+		RSAKeyPairGen.initialize(1024);
+		RSAKeyPair = RSAKeyPairGen.genKeyPair();
 
-		KeyPairGenerator keyPairGenDSA = KeyPairGenerator.getInstance("DSA");
-		keyPairGenDSA.initialize(1024);
-		keyPairDSA = keyPairGenDSA.genKeyPair();
+		KeyPairGenerator DSAKeyPairGen = KeyPairGenerator.getInstance("DSA");
+		DSAKeyPairGen.initialize(1024);
+		DSAKeyPair = DSAKeyPairGen.genKeyPair();
 
 		// EPK = public encryption key
-		pkr.setKey(groupName + "_EPK", keyPairRSA.getPublic(), "EPK");
+		pkr.setKey(groupName + "_EPK", RSAKeyPair.getPublic(), "EPK");
 		// ESK = private decryption key
-		pkr.setKey(groupName + "_ESK", keyPairRSA.getPrivate(), "ESK");
+		pkr.setKey(groupName + "_ESK", RSAKeyPair.getPrivate(), "ESK");
 		// SPK = public verification key
-		pkr.setKey(groupName + "_SPK", keyPairDSA.getPublic(), "SPK");
+		pkr.setKey(groupName + "_SPK", DSAKeyPair.getPublic(), "SPK");
 		// SSK = private signing key
-		pkr.setKey(groupName + "_SSK", keyPairDSA.getPrivate(), "SSK");
+		pkr.setKey(groupName + "_SSK", DSAKeyPair.getPrivate(), "SSK");
 		
 		System.out.println(pkr.getKey(groupName + "_EPK"));
 	}
